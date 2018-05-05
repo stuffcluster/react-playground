4e61a5b5e584c34f85a8049454a4996aaab2f8af
spitchell <mr.spitchell@gmail.com>
2018-05-05 07:50:45 +0100

    svg working

--- src/App.js
+++ src/App.js
@@ -1,54 +1,24 @@
 import React, { Component } from 'react';
-import './App.css';
-import {Picture} from './Picture.js';
-import Field from './Field.js'; 
+import YuLogo from  './YuLogo.js';
 
 class App extends Component {
   constructor(){
     super();
     this.state={
-      mouseX: 0,
-      mouseY: 0,
-      showPictures: false,
-      header: "This is the header",
-      imageURL: "http://www.dogbreedworld.com/dogs/kangal_dog/kangal_dog.jpg",
-      number: [0]
-    }
+      color: '#000',
+      viewBox: '-100 0 800 369.1',
+    };
+
   }
 
-  addPicture=()=> {
-    this.setState(prevState => ({number: [...prevState.number, 0]}))
+  updateColor=(e)=> {
+    this.setState({color: e.target.value});
   }
-
-  toggleShow=()=> {
-    this.setState(prevState => ({showPictures: !prevState.showPictures}))
-  }
-
-  mouseHandler=(e)=> {
-    const x = e.clientX.toString(16);
-    const y = e.clientY.toString(16);
-    const color = `#${x}8${y}`;
-
-    this.setState({mouseX: x, mouseY: y, color});
-  }
-
-  render() {
+  render(){
     return (
-
-      <div className="App">
-        <header style={{background: this.state.color}} className="App-header">
-          <h1>{this.state.header}</h1>
-        </header>
-          { this.state.showPictures ?
-            <div>
-              <Picture imageSource={this.state.imageURL} alt="dog" number={this.state.number}/>
-            </div>
-              :
-              <h1 style={{color: this.state.color}}>Nothing to see</h1>
-          }
-          <button onClick={this.toggleShow} >SHOW/HIDE</button>
-          <button onClick={this.addPicture}>ADD PIC</button>
-          <Field mouseHandler={this.mouseHandler} color={this.state.color} />
+      <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around", height: "100vh"}}>
+        <YuLogo {...this.state} style={{width: "50%"}}/>
+        <input style={{width: "100px", height: "100px"}} type="color" onChange={this.updateColor}/>
       </div>
     );
   }
--- src/Field.js
+++ src/Field.js
@@ -3,13 +3,14 @@ import React from 'react';
 const Field = (props) => {
   return <div 
     style={{
+        border: 'thin solid black',
       margin: '20px',
       width: '200px', 
       height: '200px',
       background: props.color 
     }}
     onMouseMove={props.mouseHandler}
-  ></div>
+  >color field</div>
 }
 
 export default Field;
